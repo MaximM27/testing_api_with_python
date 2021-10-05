c082003b2c534f91aef41db2815efc86686b3058
Maxim <makarovma27@gmail.com>
2021-10-06 02:30:20 +0300

    Скорректирован скрипт для записи ответа на http-запрос

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import requests
 print('hello from Max')
 
-response = requests.get("https://playground.learnqa.ru/api/hello")
+response = requests.get("https://playground.learnqa.ru/api/get_text")
 print(response.text)
 
